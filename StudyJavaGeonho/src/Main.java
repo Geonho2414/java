644863484d16e8ffb3acd9531cf64ea1deca4e93
Geonho2414 <geonhocho2414@gmail.com>
2024-02-29 15:23:09 +0900

    Update Main.java

--- StudyJavaGeonho/src/Main.java
+++ StudyJavaGeonho/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        int a = 5;
+        System.out.println("Hello world!");
     }
 }
 // https://github.com/hyungwonseo/java
+// 눈으로 보지 말고 진짜로 하자! 최대한 원리를 이해하고 내가 스스로 써보자
