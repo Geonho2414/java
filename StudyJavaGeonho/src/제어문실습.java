e21199c3a5ac0284995459d6391682d844a131d7
Geonho2414 <geonhocho2414@gmail.com>
2024-02-29 11:39:42 +0900

    change

--- StudyJavaGeonho/src/제어문실습.java
+++ StudyJavaGeonho/src/제어문실습.java
@@ -1,4 +1,5 @@
 public class 제어문실습 {
+
     public static void main(String[] args) {
         //if 단독사용
         boolean x = true;
@@ -8,6 +9,27 @@ public class 제어문실습 {
         if (!x) {
             System.out.println("이건 안보일걸!");
 
+        }
+        // if ~ else
+        if (x) {
+            System.out.println("x는 true");
+        } else {
+            System.out.println("x는 false");
+        }
+        System.out.println();
+
+        //if ~ else 의 중복사용
+        boolean a = false;
+        boolean b = true;
+        boolean c = true;
+        if (a) {
+            System.out.println("a가 true");
+        } else if (b) {
+            System.out.println("a는 false, b가 true");
+        } else if (c) {
+            System.out.println("a,b가 false, c가 true");
+        } else {
+            System.out.println("a,b,c는 false");
         }
     }
 }
