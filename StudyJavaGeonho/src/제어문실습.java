0993a81974c4314380ad965308bfd5b1c6a12397
Geonho2414 <geonhocho2414@gmail.com>
2024-02-29 11:26:20 +0900

    change

--- StudyJavaGeonho/src/제어문실습.java
+++ StudyJavaGeonho/src/제어문실습.java
@@ -1,6 +1,13 @@
 public class 제어문실습 {
     public static void main(String[] args) {
-        
+        //if 단독사용
+        boolean x = true;
+        if (x) {
+            System.out.println("테스트중");
+        }
+        if (!x) {
+            System.out.println("이건 안보일걸!");
 
+        }
     }
 }
