92a32db760a0348efa1db73eefa740c16666b807
Geonho2414 <geonhocho2414@gmail.com>
2024-03-04 14:51:44 +0900

    2024.03.04배열

---
+++ StudyJavaGeonho/src/배열실습/Java.java
@@ -0,0 +1,21 @@
+package 배열실습;
+
+public class Java {
+    public static void main(String[] args) {
+
+        //배열
+        // 데이터타입[]이름 = {"갯수"};
+        String[] colorList = {"black" };
+
+
+        /*String[]studentList = {"오겸비","조건호","조수아"};
+          for(int k = 0; k < studentList = 3 ; k++) {
+            if ( studentList[k] == "조건호"){
+            }
+        }*/
+
+        //배열 예시
+        int[] intArray = {1, 2, 3, 4, 5};
+        System.out.println(intArray);
+    }
+}
