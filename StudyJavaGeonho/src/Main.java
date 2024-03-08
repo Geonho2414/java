0b6e70e8b8b22ac66d359476ac26d0fa2549438d
Geonho2414 <geonhocho2414@gmail.com>
2024-03-08 12:28:22 +0900

    20240308am

--- StudyJavaGeonho/src/Main.java
+++ StudyJavaGeonho/src/Main.java
@@ -1,7 +1,39 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello");
+
+        //ch7_classTraining 실습
+        ch5_classTraining Raina = new ch5_classTraining("Raina", 21, "평택", "enfp", 040615);
+        ch5_classTraining Tom = new ch5_classTraining("Tom", 22, "대전", "entj",123456);
+        ch5_classTraining Paul = new ch5_classTraining();
+//        Paul.name = "paul";
+//        Paul.age = 23;
+//        Paul.address = "계룡";
+//        Paul.mbti = "cute";
+        Paul.setName("Paul");
+        Paul.setAge(23);
+        Paul.setAddress("계룡");
+        Paul.setMbti("cute");
+        Raina.displayAll();
+        Tom.displayAll();
+        Paul.displayAll();
+
+        // 메서드 호출
+        int[] numberList = {12, 34, 56, 32, 84, 99, 73, 69};
+        int[] numberList2 = {123,456,789,345};
+        getSumAvg(numberList);
+        getSumAvg(numberList2);
+    }
+
+    //ch7_classTraining 실습
+    static void getSumAvg(int[] numberList) {
+        int sum = 0;
+        for (int i = 0; i < numberList.length; i++) {
+            sum = sum + numberList[i];
+        }
+        int average = sum / numberList.length;
+        System.out.println(sum + " " + average);
     }
 }
 // https://github.com/hyungwonseo/java
