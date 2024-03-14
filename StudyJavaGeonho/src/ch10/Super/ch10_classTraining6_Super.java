db17b1a71da2672436ec23cf16f4e4e471810a9f
Geonho2414 <geonhocho2414@gmail.com>
2024-03-14 10:41:12 +0900

    Create ch10_classTraining6_Super.java

---
+++ StudyJavaGeonho/src/ch10/Super/ch10_classTraining6_Super.java
@@ -0,0 +1,39 @@
+package ch10.Super;
+
+class A{
+    A(){
+        System.out.println("A 생성자");
+    }
+}
+class B extends A{
+    B(){
+        super(); //생략시 컴파일러가 자동 추가 (부모클래스의 생성자 호출)
+        System.out.println("B 생성자");
+    }
+}
+class C {
+    C(int a){
+        System.out.println("C 생성자");
+    }
+}
+class D extends C {
+    /* 컴파일러가 자동으로 추가해주는 내용
+    D(){
+        super();
+    }
+    */
+    D(){
+        super(3);
+    }
+}
+
+public class ch10_classTraining6_Super {
+    public static void main(String[] args) {
+        //#1. A 객체 생성
+        A aa = new A();
+        System.out.println(); // A 생성자
+
+        //#2. B 객체 생성
+        B bb = new B(); // A 생성자 -> B 생성자
+    }
+}
